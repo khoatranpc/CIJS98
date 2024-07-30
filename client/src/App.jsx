bba514e9561d259a00fb1beee691be1eaf690890
khotranpc <khotranpc603@gmail.com>
2024-07-30 20:49:35 +0700

    lesson 3

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import Card from './Card.jsx';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hello mọi người!</h1>
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  )
+}
+
+export default App;
---
+++ client/src/Card.jsx
@@ -0,0 +1,13 @@
+import './Card.css';
+
+const Card = () => {
+    return (
+        <div className="card">
+            <h1>HHIHI</h1>
+            <div className="name">Name: MindX School</div>
+            <div className="age">Age: 10</div>
+        </div>
+    )
+}
+
+export default Card;
